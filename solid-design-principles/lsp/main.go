ee93201e5c387a862177a652361880f1dd4b8994
Kelvin Isievwore <kelvinator4leo@gmail.com>
2023-05-31 01:58:56 +0100

    Liskov Substitution Principle

---
+++ solid-design-principles/lsp/main.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+// Liskov Substitution Principle
+
+type Sized interface {
+	GetWidth() int
+	SetWidth(width int)
+	GetHeight() int
+	SetHeight(height int)
+}
+
+type Rectangle struct {
+	width int
+	height int
+}
+
+type Square struct {
+	Rectangle
+}
+
+func (r *Rectangle) GetWidth() int {
+	return r.width
+}
+
+func (r *Rectangle) SetWidth(width int) {
+	r.width = width
+}
+
+func (r *Rectangle) GetHeight() int {
+	return r.height
+}
+
+func (r *Rectangle) SetHeight(height int) {
+	r.height = height
+}
+
+// violate LSP with Square
+func NewSquare(size int) *Square {
+	sq := Square{}
+	sq.width = size
+	sq.height = size
+	return &sq
+}
+
+func (s *Square) SetWidth(width int) {
+	s.width = width
+	s.height = width
+}
+
+func (s *Square) SetHeight(height int) {
+	s.width = height
+	s.height = height
+}
+
+// fix
+type Square2 struct {
+	size int // width, height
+}
+
+func (s *Square2) Rectangle() Rectangle {
+	return Rectangle{s.size , s.size}
+}
+
+func UseIt(sized Sized) {
+	width := sized.GetWidth()
+	sized.SetHeight(10)
+	expectedArea := 10 * width
+	actualArea := sized.GetWidth() * sized.GetHeight()
+	fmt.Print("Expected an area of ", expectedArea, ". but got ", actualArea, "\n")
+}
+
+func main() {
+	rc := &Rectangle{2, 3}
+	UseIt(rc)
+
+	sq := NewSquare(5)
+	UseIt(sq)
+}
