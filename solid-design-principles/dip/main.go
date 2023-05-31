6e9be87c97e01bf0888c42d7bfb8ea1c9d8fbae3
Kelvin Isievwore <kelvinator4leo@gmail.com>
2023-05-31 01:59:17 +0100

    Dependency Inversion Principle

---
+++ solid-design-principles/dip/main.go
@@ -0,0 +1,95 @@
+package main
+
+import "fmt"
+
+// Dependency Inversion Principle
+// Higher Level Module() should no depend on Lower Level Module (Database)
+// Both should depend on abstractions
+
+type Relationship int
+
+const (
+	Parent Relationship = iota
+	Child
+	Sibling
+)
+
+type Person struct {
+	name string
+	// ....
+}
+
+type Info struct {
+	from *Person
+	relationship Relationship
+	to *Person
+}
+
+// Low level module - save data
+type Relationships struct {
+	relations []Info
+}
+
+func (r *Relationships) AddParentAndChild(parent, child *Person) {
+	r.relations = append(r.relations, Info{parent, Parent, child})
+	r.relations = append(r.relations, Info{child, Child, parent})
+}
+
+// High level module - operate on data
+type Research struct {
+	// break DIP
+	relationships Relationships
+}
+
+func (r *Research) Investigate() {
+	relations := r.relationships.relations
+	for _, rel := range relations {
+		if rel.from.name == "John" && rel.relationship == Parent {
+			fmt.Println("John has a child called ", rel.to.name)
+		}
+	}
+}
+
+// FIX
+type ResearchFix struct {
+	browser RelationshipBrowser
+}
+
+type RelationshipBrowser interface {
+	FindAllChildrenOf(name string) []*Person
+}
+
+func (r *Relationships) FindAllChildrenOf(name string) []*Person {
+	result := make([]*Person, 0)
+	for i, v := range r.relations {
+		if v.relationship == Parent && v.from.name == name {
+			result = append(result, r.relations[i].to)
+		}
+	}
+	return result
+}
+
+func (r *ResearchFix) Investigate2() {
+	for _, p := range r.browser.FindAllChildrenOf("John") {
+		fmt.Println("John has a child called ", p.name)
+	}
+}
+
+func main() {
+
+	parent := Person{"John"}
+	child1 := Person{"Chris"}
+	child2 := Person{"Matt"}
+
+	relationships := Relationships{}
+	relationships.AddParentAndChild(&parent, &child1)
+	relationships.AddParentAndChild(&parent, &child2)
+
+	r := Research{relationships}
+	r.Investigate()
+
+	// Using Fix
+	rf := ResearchFix{&relationships}
+	rf.Investigate2()
+
+}
