07b8451f613320f4e260157b8d2599ada7870843
Kelvin Isievwore <kelvinator4leo@gmail.com>
2023-11-23 19:30:43 +0100

    Dependency Inversion

---
+++ singleton/singleton-dependency-inversion/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+)
+
+type Database interface {
+	GetPopulation(city string) int
+}
+
+type singletonDatabase struct {
+	capitals map[string]int
+}
+
+func (db *singletonDatabase) GetPopulation(city string) int {
+	return db.capitals[city]
+}
+
+func readData(path string) (map[string]int, error) {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	exPath := filepath.Dir(ex)
+
+	file, err := os.Open(exPath + path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	result := map[string]int{}
+	
+	for scanner.Scan() {
+		k := scanner.Text()
+		scanner.Scan()
+		v, _ := strconv.Atoi(scanner.Text())
+		result[k] = v
+	}
+	return result, nil
+}
+
+// synce.Once or init() - Thread safety
+// laziness
+
+var once sync.Once
+var instance *singletonDatabase
+
+func GetSingletonDatabase() *singletonDatabase {
+	once.Do(func() {
+		caps, e := readData("capitals.txt")
+		db := singletonDatabase{caps}
+		if e == nil {
+			db.capitals = caps
+		}
+		instance = &db
+	})
+	return instance
+}
+
+func GetTotalPopulation(cities []string) int {
+	result := 0
+	for _, city := range cities {
+		result += GetSingletonDatabase().GetPopulation(city) // Depend on concrete implementation
+	}
+	return result
+}
+
+
+func GetTotalPopulationNew(db Database, cities []string) int {
+	result := 0
+	for _, city := range cities {
+		result += db.GetPopulation(city) // Now Depends on an interface
+	}
+	return result
+}
+
+type DummyDatabase struct {
+	dummyData map[string]int
+}
+
+func (d *DummyDatabase) GetPopulation(city string) int {
+	if len(d.dummyData) == 0 {
+		d.dummyData = map[string]int{
+			"alpha": 1,
+			"beta": 2,
+			"gamma": 3,
+		}
+	}
+	return d.dummyData[city]
+}
+
+func main() {
+	names := []string{"alpha", "gamma"}
+	tp := GetTotalPopulationNew(&DummyDatabase{}, names)
+	fmt.Println(tp == 4)
+}
