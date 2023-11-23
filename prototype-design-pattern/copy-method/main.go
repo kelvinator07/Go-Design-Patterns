641cdd256c56e9981b8940dc31d16257a3931c02
Kelvin Isievwore <kelvinator4leo@gmail.com>
2023-11-23 18:03:50 +0100

    Copy Method

---
+++ prototype-design-pattern/copy-method/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	StreetAddress, City, Country string
+}
+
+type Person struct {
+	Name string
+	Address *Address
+	Friends []string
+}
+
+func (p *Person) DeepCopy() *Person {
+	q := *p
+	q.Address = p.Address.DeepCopy()
+	copy(q.Friends, p.Friends)
+	return &q
+}
+
+func (a *Address) DeepCopy() *Address {
+	return &Address{a.StreetAddress, a.City, a.Country}
+}
+
+func main() {
+	john := Person{"Kelvin", &Address{"123 Lekki", "Lagos", "Nigeria"}, []string{"Chris", "Matt"}}
+	
+	jane := john.DeepCopy()
+	jane.Name = "Jane"
+	jane.Address.StreetAddress = "321 Ikoyi"
+	jane.Friends = append(jane.Friends, "Angela")
+
+	fmt.Println(john, john.Address)
+	fmt.Println(jane, jane.Address)
+}
